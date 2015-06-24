0dc385f323925295ea0207543187ebbbd5cdb5d3
ricochet <ricci.p.huang@gmail.com>
2015-06-24 17:23:08 -0400

    Now it gets different schools and is almost there getting the players... there's a weird problem where it randomly goes through different schools and not in order? Try to check it out.

--- scraping/getPlayers.js
+++ scraping/getPlayers.js
@@ -4,26 +4,39 @@ var cheerio = require('cheerio');
 console.log("Acquiring player list... please wait.");
 
 var count = 0;
-var schools = 2;
-for(schools = 2; schools<818;schools++){//school IDs go from 2 to 818
-	request('http://stats.ncaa.org/team/roster/12100?org_id='+schools, function (error, response, html) {
-		if (!error && response.statusCode === 200) {
-			var $ = cheerio.load(html);
-			count = 0;
-			schools++;
-			$('a').each(function(i, element){			
-				var a = $(this);
-				var identifier = a.text();
-				count++;
-				if(count===2 && identifier.charAt(0) === 'S'){//if a page has players, it'll say Schedule/Results in the second 'a' tag
-					console.log("full"+schools);
-				}
-				else if (count===2 && identifier.charAt(0) === 'T'){//if a page has no players, it'll say Terms and Conditions in the second 'a' tag
-					console.log("empty"+schools);
-				}
-
-			});
-
-		}
-	});
+var schools = new Array(1071);
+for(count=2;count<1073;count++){
+	schools[count] = count;
 }
+count = 0;
+var school = 0;
+for(school in schools){//school IDs go from 2 to 818
+	var url = 'http://stats.ncaa.org/team/roster/12100?org_id='+schools[school];
+	request(url, ( function(school) {
+		return function (error, response, html) {
+			if (!error && response.statusCode === 200) {
+				var $ = cheerio.load(html);
+				count = 0;
+				$('a').each(function(i, element){			
+					var a = $(this);
+					var identifier = a.text();
+					count++;
+					if(count===2 && identifier.charAt(0) === 'S'){//if a page has players, it'll say Schedule/Results in the second 'a' tag
+						
+						var metadata = {
+								school: $('h1').text().trim(),
+								player1: $('a').next().next().next().next().next().next().next().next().next().next().next().text()
+						};
+						console.log(metadata);
+					}
+					else if (count===2 && identifier.charAt(0) === 'T'){//if a page has no players, it'll say Terms and Conditions in the second 'a' tag
+						//console.log(school);
+					}
+
+				});
+			}
+		};
+	})(school));
+
+}
+
